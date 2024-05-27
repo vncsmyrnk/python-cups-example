db9a8f0104618bdde56aa4f5d6c0d9fcc7d58359
Vinicius Mayrink <vmayrink12@gmail.com>
2024-05-27 18:46:24 -0300

    Adding simple example page to print a report

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ WORKDIR /var/app/
 RUN <<EOF
 apt update
 apt install cups python3-dev libcups2-dev build-essential libpq-dev -y
+service cups start
 lpadmin -p hp-casa -E -v ipp://192.168.100.10/ipp/print -m everywhere
 EOF
 COPY src/ .
--- src/server.py
+++ src/server.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request
+from datetime import datetime
 import cups
 
 
@@ -24,3 +25,34 @@ def print_file():
     return {
             "job_id": job_id
     }
+
+
+@app.post("/print-report")
+def print_report():
+    unit_name = request.json["unit_name"]
+    now = datetime.now()
+    file_content = f"""-----------------------------
+UNIT: {unit_name}
+DATE: {now.strftime("%m/%d/%Y")}
+TIME: {now.strftime("%H:%M:%S")}
+-----------------------------
+"""
+
+    with open("/tmp/report.txt", "w") as file1:
+        file1.write(file_content)
+
+    conn = cups.Connection()
+    job_id = conn.printFile(
+        "hp-casa",
+        "/tmp/report.txt",
+        "Report",
+        options={}
+    )
+    return {
+            "job_id": job_id
+    }
+
+
+@app.route("/")
+def render_index():
+    return render_template("index.html")
